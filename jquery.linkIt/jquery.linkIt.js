ef99b26ad6b408112395c1306abe1e6c093263de
Scott <scott_baron@yahoo.com>
2015-12-29 02:42:33 +0200

    commit all

---
+++ jquery.linkIt/jquery.linkIt.js
@@ -0,0 +1,31 @@
+/*
+* name: LinkIt
+* author: Scott Baron
+* version: 0.1.0
+* license: MIT
+*/
+(function ($) {
+    $.fn.linkIt = function(options) {
+        var settings = $.extend({
+               href     :null,
+               text     :null,
+               target   :'_self' 
+        }, options);
+        //Validate
+        if(settings.href == null){
+            console.log('You need an href option for LinkIt to work');
+            return this;
+        }
+            return this.each(function() {
+                var object = $(this)
+
+                if(settings.text == null){
+                    settings.text = object.text();
+                }
+                     object.wrap('<a target="'+settings.target+'" href="'+settings.href+'"></a>').text(settings.text);
+
+            });
+
+    }
+
+}(jQuery));
